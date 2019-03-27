5bc3633536d64824e512272fdc440d702e7bb7af
tkhrname <takehiro.watanabe.224@gmail.com>
2019-03-27 20:34:22 +0900

    Initial Commit

---
+++ MVVM-Pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-Pattern
+//
+//  Created by 渡邊丈洋 on 2019/03/27.
+//  Copyright © 2019 渡邊丈洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
