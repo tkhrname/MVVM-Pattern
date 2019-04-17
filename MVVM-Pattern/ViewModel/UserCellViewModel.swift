5e26831c13343c5ea6c1f486b0feebf5e12adfcf
tkhrname <takehiro.watanabe.224@gmail.com>
2019-04-17 14:17:07 +0900

    Add ModelView class

---
+++ MVVM-Pattern/ViewModel/UserCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserCellViewModel.swift
+//  MVVM-Pattern
+//
+//  Created by 渡邊丈洋 on 2019/04/16.
+//  Copyright © 2019 渡邊丈洋. All rights reserved.
+//
+
+import Foundation
+
+// Cell1つ1つに対するアウトプットを担当する
+final class UserCellViewModel {
+    
+}
---
+++ MVVM-Pattern/ViewModel/UserListViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  UserListViewModel.swift
+//  MVVM-Pattern
+//
+//  Created by 渡邊丈洋 on 2019/04/16.
+//  Copyright © 2019 渡邊丈洋. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// 通信状態
+enum ViewModelState {
+    case loading
+    case finish
+    case error(Error)
+}
+
+final class UserListViewModel {
+    // ViewModelStateをclosureとしてpropertyで保持
+    // この変数がViewControllerに対して通知を送る役割を果たす
+    var stateDidUpdate: ((ViewModelState) -> Void)?
+    //userの配列
+    private var users = [User]()
+    // UserCellViewModelの配列
+    var cellViewModels = [UserCellViewModel]()
+    // Model層で定義したAPIクラスを変数として保持
+    let api = API()
+    // Userの配列を取得する
+    func getUsers() {
+        self.stateDidUpdate?(.loading)
+        self.users.removeAll()
+        
+        api.getUsers(success: { (users) in
+            self.users.append(contentsOf: users)
+        }, failure: { (error) in
+            
+        })
+    }
+}
